91b3d81d024c8f7325cc279e867c1ee6b30f50a5
Gill Abada <gill@callifi.com>
2019-01-29 16:00:22 -0500

    Add four more listeners.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,25 @@ busImg.addEventListener('mouseover', e => {
 });
 
 const navBus = document.querySelector('.logo-heading');
-console.log(navBus);
 window.addEventListener('scroll', e => {
     navBus.textContent = "Struggle Bus"
 });
+
+const welcomeText = document.querySelector('h2');
+welcomeText.addEventListener('dblclick', e => {
+    welcomeText.textContent = "Welcome to the STRUGGLE BUS!"
+});
+
+const adventureImg = document.querySelectorAll('.img-content img');
+adventureImg[0].addEventListener('drag', e => {
+    adventureImg[0].setAttribute('src', 'img/sad_face.jpg')
+})
+
+window.addEventListener('resize', e => {
+    adventureImg[1].setAttribute('src', 'img/angry.jpg')
+})
+
+const secondSlide = document.querySelector('.content-destination img');
+secondSlide.addEventListener('contextmenu', e => {
+    secondSlide.setAttribute('src', 'img/alone.jpg')
+})
