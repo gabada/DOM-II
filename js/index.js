6d985963f3a779b89ff5455d1d6120c2aee51d24
Gill Abada <gill@callifi.com>
2019-01-29 15:40:39 -0500

    add events for mouseover and scroll. also add a new img.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const busImg = document.querySelector('img');
+busImg.addEventListener('mouseover', e => {
+    busImg.setAttribute('src', 'img/fire_s_bus.png')
+});
+
+const navBus = document.querySelector('.logo-heading');
+console.log(navBus);
+window.addEventListener('scroll', e => {
+    navBus.textContent = "Struggle Bus"
+});
